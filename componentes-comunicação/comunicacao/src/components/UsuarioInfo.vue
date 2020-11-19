80c574f761b8e9f817067a99264979f6d2fe2d1e
Felipe Lelis <felipe@atelco.com.br>
2020-11-19 14:52:10 -0300

    primeiro commit

---
+++ componentes-comunicação/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p>Vários detalhes...</p>
+    <p>Nome do Usuário : <strong>{{ inverterNome() }}</strong></p>
+    <button @click="reiniciarNome">Reiniciar Nome</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nome : {
+      type : String,
+      // required : true
+      required : 'Anônimo'
+      // default : function () {
+      //   return Array(10).fill(0). join(',')
+      // }
+    }
+  },
+  data(){
+    return {
+
+    }
+  },
+  methods : {
+    inverterNome() {
+      return this.nome.split('').reverse().join('')
+    },
+    reiniciarNome() {
+      this.nome = 'Pedro'
+      this.$emit('nomeMudou', this.nome)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
+</style>
